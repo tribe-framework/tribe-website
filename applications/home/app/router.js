94139c393eb88cb911d7cd8d0475dcb6d44593a2
akshaymadan <akshay.madan@ez.works>
2026-06-29 16:02:41 +0530

    mysql performance tuning

--- applications/home/app/router.js
+++ applications/home/app/router.js
@@ -18,4 +18,5 @@ Router.map(function () {
   this.route('vhost');
   this.route('metadata');
   this.route('env');
+  this.route('mysql');
 });
---
+++ applications/home/app/routes/mysql.js
@@ -0,0 +1,3 @@
+import Route from '@ember/routing/route';
+
+export default class MysqlRoute extends Route {}
